e6732755fc3e903b1012f67cb3f0f2c7c6e7f934
DicoAji <dicoji12@gmail.com>
2022-12-10 02:48:27 +0700

    ubah lorem

--- assets/script.js
+++ assets/script.js
@@ -24,3 +24,39 @@ $(document).ready(function () {
     } // End if
   });
 });
+
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".slide-right", {
+  duration: 3000,
+  origin: "left",
+  distance: "300px",
+  // easing: "ease-in-out"
+});
+ScrollReveal().reveal(".slide-left", {
+  duration: 3000,
+  origin: "right",
+  distance: "300px",
+  // easing: "ease-in-out"
+});
+ScrollReveal().reveal(".slide-top", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "300px",
+  // easing: "ease-in-out"
+});
+ScrollReveal().reveal(".slide-bottom", {
+  duration: 3000,
+  origin: "top",
+  distance: "300px",
+  // easing: "ease-in-out"
+});
+
+ScrollReveal().reveal(".slide-up", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "100px",
+  easing: "cubic-bezier(.37,.01,.74,1)",
+  opacity: 0.3,
+  scale: 0.5,
+});
